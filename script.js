164c6ea82e97767005267e1a81198228a03f0392
Jesse Chase <93272982+Jess-EXE@users.noreply.github.com>
2022-04-04 20:06:16 -0500

    Edited site.js, app.html

--- script.js
+++ script.js
@@ -17,14 +17,12 @@ let contX = [];
 let unusedX = [];
 
 
-function addDx() {
-    let select = document.getElementById("inputState");
-    let value = select.options[select.selectedIndex].value;
-    console.log(value);
+const addDx = (ev) => {
+    ev.preventDefault();
     let diagnosis = {
         //id: Date.now(),
         dx: document.getElementById('dx').value,
-        cat: value
+        cat: document.querySelector('input[name="cat"]:checked').value
     }
     diagnoses.push(diagnosis);
     document.querySelector('form').reset();
@@ -93,87 +91,87 @@ function completeCarePlan() {
     }
 
     if (cardX.length > 0) {
-        document.getElementById('card-x').classList.toggle('invisible');
-        let cardDx = document.getElementById('cardDx');
+        document.getElementById('card-x').classList.toggle('hide');
+        let cardDx = document.querySelector('#cardDx');
         cardDx.textContent = cardX;
     }
     if (respX.length > 0) {
-        document.getElementById('resp-x').classList.toggle('invisible');
+        document.getElementById('resp-x').classList.toggle('hide');
         let respDx = document.querySelector('#respDx');
         respDx.textContent = respX;
     }
     if (labsX.length > 0) {
-        document.getElementById('labs-x').classList.toggle('invisible');
+        document.getElementById('labs-x').classList.toggle('hide');
         let labsDx = document.querySelector('#labsDx');
         labsDx.textContent = labsX;
     }
     if (painX.length > 0) {
-        document.getElementById('pain-x').classList.toggle('invisible');
+        document.getElementById('pain-x').classList.toggle('hide');
         let painDx = document.querySelector('#painDx');
         painDx.textContent = painX;
     }
     if (adlsX.length > 0) {
-        document.getElementById('adls-x').classList.toggle('invisible');
+        document.getElementById('adls-x').classList.toggle('hide');
         let adlsDx = document.querySelector('#adlsDx');
         adlsDx.textContent = adlsX;
     }
     if (consX.length > 0) {
-        document.getElementById('cons-x').classList.toggle('invisible');
+        document.getElementById('cons-x').classList.toggle('hide');
         let consDx = document.querySelector('#consDx');
         consDx.textContent = consX;
     }
     if (diabetesX.length > 0) {
-        document.getElementById('diabetes-x').classList.toggle('invisible');
+        document.getElementById('diabetes-x').classList.toggle('hide');
         let diabetesDx = document.querySelector('#diabetesDx');
         diabetesDx.textContent = diabetesX;
     }
     if (coldX.length > 0) {
-        document.getElementById('cold-x').classList.toggle('invisible');
+        document.getElementById('cold-x').classList.toggle('hide');
         let coldDx = document.querySelector('#coldDx');
         coldDx.textContent = coldX;
     }
     if (nutritionX.length > 0) {
-        document.getElementById('nutrition-x').classList.toggle('invisible');
+        document.getElementById('nutrition-x').classList.toggle('hide');
         let nutritionDx = document.querySelector('#nutritionDx');
         nutritionDx.textContent = nutritionX;
     }
     if (skinX.length > 0) {
-        document.getElementById('skin-x').classList.toggle('invisible');
+        document.getElementById('skin-x').classList.toggle('hide');
         let skinDx = document.querySelector('#skinDx');
         skinDx.textContent = skinX;
     }
     if (giX.length > 0) {
-        document.getElementById('gi-x').classList.toggle('invisible');
+        document.getElementById('gi-x').classList.toggle('hide');
         let giDx = document.querySelector('#giDx');
         giDx.textContent = giX;
     }
     if (visionX.length > 0) {
-        document.getElementById('vision-x').classList.toggle('invisible');
+        document.getElementById('vision-x').classList.toggle('hide');
         let visionDx = document.querySelector('#visionDx');
         visionDx.textContent = visionX;
     }
     if (confX.length > 0) {
-        document.getElementById('conf-x').classList.toggle('invisible');
+        document.getElementById('conf-x').classList.toggle('hide');
         let confDx = document.querySelector('#confDx');
         confDx.textContent = confX;
     }
     if (moodX.length > 0) {
-        document.getElementById('mood-x').classList.toggle('invisible');
+        document.getElementById('mood-x').classList.toggle('hide');
         let moodDx = document.querySelector('#moodDx');
         moodDx.textContent = moodX;
     }
     if (contX.length > 0) {
-        document.getElementById('cont-x').classList.toggle('invisible');
+        document.getElementById('cont-x').classList.toggle('hide');
         let contDx = document.querySelector('#contDx');
         contDx.textContent = contX;
     }
     if (unusedX.length > 0) {
-        document.getElementById('unused-x').classList.toggle('invisible');
+        document.getElementById('unused-x').classList.toggle('hide');
         let unusedDx = document.querySelector('#unusedDx');
         unusedDx.textContent = unusedX;
     }
 }
 
-// document.addEventListener('DOMContentLoaded', () =>  {
-//     document.getElementById('btn').addEventListener('click', addDx);
-// })
+document.addEventListener('DOMContentLoaded', () =>  {
+    document.getElementById('btn').addEventListener('click', addDx);
+})
